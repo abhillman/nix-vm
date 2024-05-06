a085b878d5a0d601ade826c6f2ad4c922a85b550
Aryeh Hillman <aryeh.hillman@gmail.com>
2024-05-05 20:35:23 -0700

    Automatically login via getty

--- configuration.nix
+++ configuration.nix
@@ -3,15 +3,18 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  users.users.alice = {
+  users.users.aryehh = {
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [
       cowsay
       lolcat
     ];
-    initialPassword = "test";
+    initialPassword = "password";
   };
+
+
+  services.getty.autologinUser = "aryehh";
 
   services.openssh.enable = true;
   services.openssh.settings.X11Forwarding = true;
